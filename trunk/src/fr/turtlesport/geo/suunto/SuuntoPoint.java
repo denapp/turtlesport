a92a2fa0ad2f13919b595bb034928f228fa00c1d
apparicio <apparicio@221864b1-cc03-4442-bad5-58417060d638>
2012-10-17 12:29:50 +0000

    Support montre Suunto Ambit

---
+++ trunk/src/fr/turtlesport/geo/suunto/SuuntoPoint.java
@@ -0,0 +1,56 @@
+package fr.turtlesport.geo.suunto;
+
+import fr.turtlesport.geo.GeoPositionWithAlt;
+
+/**
+ * @author Denis Apparicio
+ * 
+ */
+public class SuuntoPoint extends GeoPositionWithAlt {
+
+  private double temperature       = 0;
+
+  private double energyConsumption = 0;
+
+  public SuuntoPoint() {
+    super();
+  }
+
+  /**
+   * @param latitude
+   * @param longitude
+   * @param elevation
+   */
+  public SuuntoPoint(double latitude, double longitude, double elevation) {
+    super(latitude, longitude, elevation);
+  }
+
+  /**
+   * @param latitude
+   * @param longitude
+   */
+  public SuuntoPoint(double latitude, double longitude) {
+    super(latitude, longitude);
+  }
+
+  public double getTemperature() {
+    return temperature;
+  }
+
+  public void setTemperature(double temperature) {
+    this.temperature = temperature;
+  }
+
+  public double getEnergyConsumption() {
+    return energyConsumption;
+  }
+
+  /**
+   * Energy en kj par seconde ( /4184 pour conversion en calories)
+   * @param energyConsumption
+   */
+  public void setEnergyConsumption(double energyConsumption) {
+    this.energyConsumption = energyConsumption;
+  }
+
+}
