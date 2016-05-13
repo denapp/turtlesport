4df004a0742fb12f77c7cefa804df6bd1112d260
Denis Apparicio <28752913+denapp@users.noreply.github.com>
2016-05-13 16:54:26 +0000

    version 1.9

--- src/fr/turtlesport/Version.java
+++ src/fr/turtlesport/Version.java
@@ -11,7 +11,7 @@ public final class Version {
   }
   
   /** Version de l'application. */
-  public static final String VERSION = "1.8";
+  public static final String VERSION = "1.9";
   
   /** Version majeure. */
   public static final String VERSION_MAJOR = "1";
