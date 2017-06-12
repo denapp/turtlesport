2313382f1bc4788e10151035dcfd44b03e4e75ff
apparicio <apparicio@221864b1-cc03-4442-bad5-58417060d638>
2017-06-12 19:09:21 +0000

    2.0

--- trunk/src/fr/turtlesport/Version.java
+++ trunk/src/fr/turtlesport/Version.java
@@ -11,12 +11,12 @@ public final class Version {
   }
   
   /** Version de l'application. */
-  public static final String VERSION = "1.9";
+  public static final String VERSION = "2.0";
   
   /** Version majeure. */
-  public static final String VERSION_MAJOR = "1";
+  public static final String VERSION_MAJOR = "2";
   
   /** Version mineure. */
-  public static final String VERSION_MINOR = "9";
+  public static final String VERSION_MINOR = "0";
 
 }
