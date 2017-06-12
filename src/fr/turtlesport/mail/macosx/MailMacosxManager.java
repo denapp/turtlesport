10eb38ba8cb45c10ceae51913985b43bdd65a104
Denis Apparicio <28752913+denapp@users.noreply.github.com>
2017-06-12 18:23:50 +0000

    2.0

--- src/fr/turtlesport/mail/macosx/MailMacosxManager.java
+++ src/fr/turtlesport/mail/macosx/MailMacosxManager.java
@@ -6,75 +6,74 @@ import fr.turtlesport.util.OperatingSystem;
 
 /**
  * @author Denis Apparicio
- * 
  */
 public final class MailMacosxManager implements IMailManager {
 
-  public MailMacosxManager() {
-    if (!OperatingSystem.isMacOSX()) {
-      throw new IllegalAccessError();
+    public MailMacosxManager() {
+        if (!OperatingSystem.isMacOSX()) {
+            throw new IllegalAccessError();
+        }
     }
-  }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see fr.turtlesport.mail.IMailManager#isConfigurable()
-   */
-  public boolean isConfigurable() {
-    return false;
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see fr.turtlesport.mail.IMailManager#getMailClients()
-   */
-  public IMailClient[] getMailClients() {
-    IMailClient[] mailClients = new IMailClient[1];
-
-    mailClients[0] = new MailClientMailApp();
-    return mailClients;
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see fr.turtlesport.mail.IMailManager#hasSystemDefaultMailAvalaible()
-   */
-  public boolean hasSystemDefaultMailAvalaible() {
-    return true;
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see fr.turtlesport.mail.IMailManager#getDefaultMail()
-   */
-  public IMailClient getDefaultMail() {
-    if (MailClientMailApp.isAvailable()) {
-      return new MailClientMailApp();
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.turtlesport.mail.IMailManager#isConfigurable()
+     */
+    public boolean isConfigurable() {
+        return false;
     }
-    return new MailClientMacosxDefault();
-  }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see fr.turtlesport.mail.IMailManager#getMailClientNames()
-   */
-  public String[] getMailClientNames() {
-    String[] names = { "OS X Mail" };
-    return names;
-  }
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.turtlesport.mail.IMailManager#getMailClients()
+     */
+    public IMailClient[] getMailClients() {
+        IMailClient[] mailClients = new IMailClient[1];
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see fr.turtlesport.mail.IMailManager#defaultMail()
-   */
-  public String defaultMailName() {
-    return (MailClientMailApp.isAvailable()) ? "OS X Mail" : null;
-  }
+        mailClients[0] = new MailClientMailApp();
+        return mailClients;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.turtlesport.mail.IMailManager#hasSystemDefaultMailAvalaible()
+     */
+    public boolean hasSystemDefaultMailAvalaible() {
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.turtlesport.mail.IMailManager#getDefaultMail()
+     */
+    public IMailClient getDefaultMail() {
+        if (MailClientMailApp.isAvailable()) {
+            return new MailClientMailApp();
+        }
+        return new MailClientMacosxDefault();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.turtlesport.mail.IMailManager#getMailClientNames()
+     */
+    public String[] getMailClientNames() {
+        String[] names = {"OS X Mail"};
+        return names;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.turtlesport.mail.IMailManager#defaultMail()
+     */
+    public String defaultMailName() {
+        return (MailClientMailApp.isAvailable()) ? "OS X Mail" : null;
+    }
 
 }
